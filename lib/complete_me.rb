5e2890849251f7ddd387c9919d84eccdf13a5a6d
Sal Espinosa <sespinos@gmail.com>
2016-02-16 21:12:09 -0700

    Add insert method and test

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -1,28 +1,55 @@
+require 'pry'
+require_relative 'node'
+
 class CompleteMe
+  attr_reader :root
+
   def initialize
-
+    @letters =* ("a".."z")
+    @root    = Node.new
   end
 
-  def insert(word)
-    word_array = word.split('')
-
+  def insert(word, node = @root, index = 0)
+    letters_as_numbers = translate_word_to_index_positions(word)
+    insert_letters(letters_as_numbers)
   end
 
-  def count
+  # def count
+  #
+  # end
+  #
+  #
+  # def suggest(prefix)
+  #
+  # end
+  #
+  # def populate(words)
+  #
+  # end
+  #
+  # def select
+  #
+  # end
 
+  def translate_word_to_index_positions(word)
+    word = word.split("")
+    index_array = word.map do |letter|
+      @letters.index(letter)
+    end
   end
 
+  def insert_letters(letters_as_numbers, node = @root, index = 0)
+    next_letter = letters_as_numbers[index]
 
-  def suggest(prefix)
-
+    if next_letter == nil
+      node.set_end_of_word
+    elsif node.pointer_array[next_letter] == nil
+      node.pointer_array[next_letter] = Node.new
+      index += 1
+      insert_letters(letters_as_numbers, node.pointer_array[next_letter], index)
+    else
+      index += 1
+      insert_letters(letters_as_numbers, node.pointer_array[next_letter], index)
+    end
   end
-
-  def populate(words)
-
-  end
-
-  def select
-
-  end
-
 end
--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -14,21 +14,23 @@ class CompleteMeTest < Minitest::Test
   end
 
   def test_it_can_insert_single_words
-    
-
+    skip
   end
 
   def test_it_can_count_the_number_of_words_inserted
     skip
-
-
   end
 
   def test_it_has_a_head_with_all_letters_as_children
     skip
-
-
   end
 
+  def test_it_translates_words_to_indices
+    complete = CompleteMe.new
+
+    actual = complete.translate_word_to_index_positions("baby")
+    expected = [1, 0, 1, 24]
+    assert_equal expected, actual
+  end
 
 end
